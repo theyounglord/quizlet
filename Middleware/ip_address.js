5964cc4ed220b30f655fb08577809f22fabadcb0
Deepam Kumar <kumardeepam8600@gmail.com>
2022-06-19 09:15:43 +0530

    Delete ip_address.js

--- Middleware/ip_address.js
+++
@@ -1,6 +0,0 @@
-// require all models from the models folder
-const Room = require('../../Models/room/room');
-const Admin = require('../../Models/admin/admin');
-const User = require('../../Models/user/user');
-// const Quiz = require('../../Models/quiz/quiz');
-const Question = require('../../Models/room/questions');
